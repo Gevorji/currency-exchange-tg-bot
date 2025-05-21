a3a4f018b7d322e9b18f50e8c82a518f45df5edd
Gevorji <techno9882@gmail.com>
2025-05-21 11:49:49 +0300

    Add comment description of options in config.py.

--- src/currency_exchange_tg_bot/config.py
+++ src/currency_exchange_tg_bot/config.py
@@ -6,15 +6,20 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Sqlite3Settings(BaseSettings):
+    # a name of a file that is used by db to connect
     connection_uri: str = "accesstoken.sqlite3"
 
 
 class TgBotSettings(BaseSettings):
     model_config = SettingsConfigDict(env_file='.env', extra='ignore')
 
+    # token must be set in .env file
     tg_bot_token: str
+    # change this if your file with admin chats ids is named differently
     admin_records_file: Path = 'admin_records'
+    # on /start command, should the bot send chat id of this private chat with user
     send_chat_ids_on_start: bool = False
+    # should the bot send a report whenever an error occurs trying to handle an update
     notify_admins_on_error: bool = True
     log_level: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"] = "INFO"
 
@@ -25,6 +30,8 @@ class CurrencyExchangeApiSettings(BaseSettings):
     username: Optional[str] = None
     password: Optional[str] = None
     host: Optional[HttpUrl] = None
+
+    # request timeout is set on each request to service api
     request_timeout: Optional[float] = 10.0
 
     @field_validator('host', mode='after')
